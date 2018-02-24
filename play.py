1cffe3d5a6c39a38285593846c6f850453224b2c
Konstantinos Tsiakas <konstantinos.tsiakas@mavs.uta.edu>
2018-02-24 14:02:29 -0600

    Update play.py

--- play.py
+++ play.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from naoqi import ALProxy
+#from naoqi import ALProxy
 import numpy as np 
 import readchar
 import time
@@ -23,13 +23,11 @@ from keras.models import load_model
 from scipy.spatial.distance import euclidean
 import itertools
 
-
 # initialize random seed
 random.seed(time.time())
 
 # NAO parameters
 ROBOT_IP = "129.107.118.226" 
-
 tts = ALProxy("ALTextToSpeech", ROBOT_IP, 9559)
 memory = ALProxy("ALMemory", ROBOT_IP, 9559)
 aup = ALProxy("ALAudioPlayer", ROBOT_IP, 9559)
@@ -61,7 +59,6 @@ def normalize_by_range(x, nmin = 0, nmax = 1):
 
 def ewma(Y, a = 0.2): 
 	S = []
-	#print Y
 	for i, y in enumerate(Y): 
 		if i == 0: 
 			S.append(y)
@@ -181,12 +178,15 @@ def assessment(server,folder):
 
 		diff_level = "Level" + str(level)
 		tts.say(diff_level)
+		tts.runAndWait()
 		time.sleep(0.5)
 
-		seq = list(np.random.choice(L, D[level-1]))		#announce sequence
+		#announce sequence
+		seq = list(np.random.choice(L, D[level-1]))		
 		for item in seq:
 			time.sleep(0.8)
 			tts.say(item)
+			tts.runAndWait()
 		aup.playSine(1000, 100, 0, 1)
 		time.sleep(1)
 
@@ -202,7 +202,7 @@ def assessment(server,folder):
 		# start time to measure response time
 		start_time = time.time()
 
-		################### CHECK USER RESPONSE AND CALCULATE SCORE ####################
+		# CHECK USER RESPONSE AND CALCULATE SCORE 
 		sig2 = 1
 		first = 0 
 		res = []	
@@ -262,12 +262,12 @@ def assessment(server,folder):
 
 	UM0 = [0.99, 0.8,  0.39, 0.05, 0.33, 0.35, 0.36, 0.36]
 	UM1 = [0.99, 0.93, 0.57, 0.54, 0.31, 0.31, 0.33, 0.34]
-	UM2 = [0.97, 0.94, 0.66, 0.15, 0.37, 0.38, 0.39, 0.4]
+	UM2 = [0.97, 0.94, 0.66, 0.15, 0.37, 0.38, 0.39, 0.4 ]
 	DIST = [euclidean(UM, UM0), euclidean(UM, UM1), euclidean(UM, UM2)]
 	
 	print DIST, DIST.index(min(DIST))
 
-	raw_input("Agree with cluster? ")
+	raw_input("Agree with cluster?")
 	
 	return DIST.index(min(DIST)), m1, m2
 
@@ -287,14 +287,15 @@ def training(server,cond, folder, um, m1, m2):
 	states, normed_states, actions, actions_oh = state_action_space()
 	start_state = (0,0,0)
 	start_state_index = states.index(tuple(start_state))
+	previous_state = []
+	previous_result = 0
 
-	# define MDP and q_table
+	# define MDP, learning and q_table
 	m = MDP(start_state, actions)
 	m.states = states
 	To = 1.0
 	gamma = 0.5
-	alpha = 0.5
-	 
+	alpha = 0.5	 
 	table = Representation('qtable', [m.actlist, m.states])
 	Q = np.asarray(table.Q)
 	ins = open(q,'r')
@@ -304,13 +305,15 @@ def training(server,cond, folder, um, m1, m2):
 	egreedy = Policy('softmax',To)
 	learning = Learning('qlearn', [alpha, gamma])
 
+	# session parameters
 	t = 1
 	turns = 20
+	state = start_state
 	while t<turns:
-		## record EEG signals when robot announces the sequence ##
+		# record EEG signals when robot announces the sequence 
 		out = open(folder + "/training_l_" + str(t), 'w')
 		server.f = out
-
+		
 		# SELECT ACTION 
 		if condition == 1: # random actions
 			if first: 
@@ -325,6 +328,7 @@ def training(server,cond, folder, um, m1, m2):
 				egreedy.Q_state = Q[state_index][:]
 			action = egreedy.return_action()	
 	
+
 		# PROVIDE FEEDBACK - IF NEEDED
 		rf = 0 	
 		if action == 4: 
@@ -369,10 +373,24 @@ def training(server,cond, folder, um, m1, m2):
 		out = open(folder + "/training_r_" + str(t), 'w')
 		server.f = out
 
+		# get engagement
+		eegfile = open(folder + "/assessment_" + str(t), 'r')
+		eng = estimate_engagement(eegfile)
+
+		# normalize EEG engagement 
+		if min(eng) < m1: 
+			m1 = min(eng)
+		if max(eng) > m2: 
+			m2 = max(eng)
+
+		eng = normalize_by_range(eng, m1, m2)
+		engagement = eng.mean()
+		feedback = engagement 
+
 		# start time to measure response time
 		start_time = time.time()
 
-		# CHECK USER RESPONSE AND CALCULATE SCORE
+		# CHECK USER RESPONSE AND SCORE
 		sig2 = 1
 		first = 0 
 		res = []	
@@ -388,22 +406,30 @@ def training(server,cond, folder, um, m1, m2):
 		if seq != res:
 			success = -1
 			score = success
-			pr = -1*length
+			result = -1*length
 		else: 
 			success = 1
 			score = length
-			pr = score
-
-		if condition == 2: # RL update 
-		#	next_state = list()
-		#	next_state_index = states.index(tuple(next_state))
-		#	reward = float(score)
-		
-		t += 1	
-	########################################################################################
+			result = score
 	
+		## record EEG signals when user presses the buttons ##
+		out = open(folder + '/dump', 'w')
+		server.f = out
+		######################################################
+		
+		reward = result if result >= 0 else -1.0
+		reward += beta*feedback
 
-
+		if condition == 2:
+			next_state = (length, rf, int(previous_result)) 
+			state_index = states.index(tuple(start_state))
+			Q[state_index][:], error = learning.update(state_index, action, next_state_index, 0, reward, Q[state_index][:], Q[next_state_index][:], done)
+		
+		previous_result = result
+		previous_state = state 
+		previous_state_index = state_index	
+		t += 1
+	
 # user info and folders
 user = raw_input('Enter userID: ')
 condition = int(raw_input('Enter condition (1 or 2): '))
@@ -412,9 +438,11 @@ if not os.path.exists(user_folder):
 	os.makedirs(user_folder)
 intro = open(user_folder + "/intro", 'w')
 
+server = 0
+m1 = 0
+m2 = 1 
 
-############ TODO MAIN #####################
-
+# MAIN 
 server = mps.initialize(intro)
 server.start()
 
@@ -424,161 +452,3 @@ time.sleep(0.5)
 
 um, m1, m2 = assessment(server, user_folder)
 TR = training(server, condition, user_folder, um, m1, m2)
-
-
-
-"""			
-rf = 0
-server = []
-s = 0 
-correct = 0 
-total = 0 
-
-D = [3,5,7,9]
-L = ('a','b','c')
-Actions = ["Easy", "Medium", "Hard", "Positive Feedback", "Negative Feedback"]
-
-dirname = 'data/user_' + user + '/session_' +  str(session_id) + '/' 
-turn = 1
-game = 1
-
-# 12 sequences of predefined actions for data collection -- show distribution of difficulty levels
-action_seqs = [[1,2,4,3,2,1,0,2,3,5,3,4,1,1,0,5,2,3,2,1,4,0,4,3,2], 
-	       [0,1,2,4,2,3,5,1,4,0,0,2,4,3,5,2,1,4,0,1,3,5,4,1,0], 
-	       [2,3,4,1,1,0,5,2,1,0,4,1,5,2,3,4,3,2,2,1,5,2,0,4,1], 				      		
-	       [2,2,5,3,1,4,1,0,5,3,4,2,1,0,4,1,1,3,5,3,1,0,2,2,4], 
-	       [2,4,3,2,1,0,4,1,5,0,4,0,1,5,2,4,2,3,5,1,4,2,3,5,3], 
-	       [2,3,5,3,1,0,4,1,0,4,1,2,2,5,1,2,4,3,2,5,2,1,0,4,2], 
-	       [0,1,2,3,4,2,0,5,1,4,1,0,5,2,3,5,1,2,3,4,2,1,5,1,2], 
-	       [1,0,1,5,2,3,2,5,1,0,4,1,5,2,1,0,5,2,3,4,2,3,2,4,1], 
-	       [0,1,2,4,3,5,2,1,0,4,1,5,2,3,5,0,1,2,4,2,3,4,0,1,1], 
-	       [1,0,2,3,4,1,0,1,5,2,4,3,5,2,1,0,2,4,3,5,2,1,4,2,3], 
-	       [0,1,4,2,3,4,1,0,4,2,0,5,1,2,3,4,2,0,5,1,0,2,5,2,1], 
-	       [0,1,2,5,1,0,4,2,3,5,3,1,5,0,4,2,5,2,3,2,1,4,3,2,3]]
-
-
-#sessID = randint(0,11)
-seqID = int(raw_input("Enter sequence ID: "))
-actions = action_seqs[seqID]
-print "seqID = " + str(seqID)
-
-ps = 0 
-total_score = 0 
-previous_score = 0 
-out = open(dirname + "/output", 'w')
-log = open(dirname + "/state_EEG", 'a')
-log2 = open(dirname + "/logfile", 'a')
-server = mps.initialize(out)
-server.start()
-
-while (turn<=len(actions)): 	
-	if not os.path.exists(dirname):
-		os.makedirs(dirname) 
-		
-	response = []
-	res = []
-	rf = 0
-
-	# select action from predefined
-	action = actions[turn-1]
-
-	## record EEG signals when robot announces the sequence ##
-	out = open(dirname + "/robot_" + str(turn), 'w')
-	server.f = out 
-	##########################################################
-
-	if action == 4: 
-		rf = 1
-		seq = list(np.random.choice(L, Dold))
-	elif action == 5: 
-		rf = 2
-		seq = list(np.random.choice(L, Dold))
-	else: 
-		seq = list(np.random.choice(L, D[action]))
-		Dold = D[action]
-
-	length = len(seq)
-	
-	r = randint(0,3)
-	if rf == 1: 
-		if previous_success == 1: 
-			tts.say(positive_success[r])
-		else: 
-			tts.say(positive_failure[r])
-
-	if rf == 2: 
-		if previous_success == 1: 
-			tts.say(negative_success[r])
-		else: 
-			tts.say(negative_failure[r])
-
-	# announce difficulty level
-	time.sleep(0.5)
-	diff_level = "Level" + str(D.index(length)+1)
-	tts.say(diff_level)
-	time.sleep(0.5)
-	
-	#announce sequence
-	for item in seq:
-		time.sleep(0.8)
-		tts.say(item)
-
-	aup.playSine(1000, 100, 0, 1)
-
-	time.sleep(1)
-
-	## record EEG signals when user presses the buttons ##
-	out = open(dirname + "/user_" + str(turn), 'w')
-	server.f = out
-	######################################################
-
-	# start time to measure response time
-	start_time = time.time()
-
-	################### CHECK USER RESPONSE AND CALCULATE SCORE ####################
-	sig2 = 1
-	first = 0 	
-	while(sig2):
-		res.append(readchar.readchar().lower())
-		if first == 0: 
-			reaction_time = time.time() - start_time
-			first = 1
-		if len(res) == Dold:
-			sig2 = 0 
-	
-	completion_time = time.time() - start_time
-	if seq != res:
-		success = -1
-		score = -1*(D.index(length)+1)
-	else: 
-		success = 1
-		score = D.index(length) + 1
-		correct += 1
-	#################################################################################
-
-	print "Turn No. " + str(turn) + " diff: " + str(diff_level) + " sequence: " +  str(seq) + " user's: " + str(res)
-
-	dataline = str(length) + ' ' + str(rf) + ' ' + str(previous_score) + ' robot_' + str(turn) + ' user_' + str(turn) + '\n'	
-	log.write(dataline)
-	dataline = str(turn) + ' ' + str(length) + ' ' + str(rf) + ' ' + str(score) + ' ' + str(success) + ' ' + str(reaction_time) + ' ' + str(completion_time) + ' ' + str(seq) + ' ' + str(res) + '\n'
-	log2.write(dataline)
-
-	previous_success = success
-	previous_score = score
-	previous_rf = rf
-
-	total_score += score
-	turn += 1
-		
-out.close()
-log.close()	
-log2.close()
-server.stop()
-
-tts.say("That's the end of our session! Please take some time to complete a survey!")
-time.sleep(0.5)
-tts.say("Thank you for your time!! Hope to see you again!!!")
-
-url = "https://form.jotform.us/72536243026148"
-webbrowser.open_new(url)
-"""
